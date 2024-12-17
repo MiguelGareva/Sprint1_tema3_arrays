a037db888f1a56df20fe168748fd7aeecfb14b91
miguel garcia <miguel.gareva@gmail.com>
2024-12-17 13:10:26 +0100

    array_filter used

--- 1.3.2_arrays_nivell3.php
+++ 1.3.2_arrays_nivell3.php
@@ -2,9 +2,13 @@
 
 declare(strict_types=1);
 
-function esPar(string $string) : bool{
-    return strlen(string : $string) % 2 == 0;
+function esPar(string $palabra) : bool{
+    return strlen(string : $palabra) % 2 == 0;
 }
 
+$arrayPalabras = array("no", "asgo", "cazcalear", "axiomatico", "concubinato");
+$arrayPares = array_filter($arrayPalabras, 'esPar');
+
+print_r($arrayPares);
 
 ?>
