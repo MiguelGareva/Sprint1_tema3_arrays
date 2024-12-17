af68fead0c10ca6040dc28577987c33a3c29478c
miguel garcia <miguel.gareva@gmail.com>
2024-12-17 12:54:41 +0100

    second part, array_map used to calculate the cubes of arrayNumeros numbers

--- 1.3.1_arrays_nivell3.php
+++ 1.3.1_arrays_nivell3.php
@@ -5,4 +5,8 @@ declare(strict_types=1);
 function calcularCubo(float $numero): float{
     return $numero ** 3;
 }
+
+$arrayNumeros = array(2, 5, 6, 10, 25);
+$arrayCubos = array_map('calcularCubo', $arrayNumeros);
+print_r($arrayCubos);
 ?>
