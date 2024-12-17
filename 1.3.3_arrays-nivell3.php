5a54e82240138481fbe5f3ff1c57d8ef71beecbc
miguel garcia <miguel.gareva@gmail.com>
2024-12-17 13:44:44 +0100

    esPrimo function developed

--- 1.3.3_arrays-nivell3.php
+++ 1.3.3_arrays-nivell3.php
@@ -2,5 +2,15 @@
 
 declare(strict_types=1);
 
-
+function esPrimo(int $numero) : bool{
+    if ($numero < 2) {
+        return false;
+    }
+    for ($i=2; $i <= sqrt($numero) ; $i++) { 
+        if($numero % $i == 0){
+            return false;
+        }
+    }
+    return true;
+}
 ?>
